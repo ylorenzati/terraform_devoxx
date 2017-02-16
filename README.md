09f5715b30d5cd30bf1462e9e091d589b8ee0776
Yannick Lorenzati <yannick.lorenzat@gmail.com>
2017-02-16 21:08:01 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # terraform_devoxx
+
+docker run -i -t hashicorp/terraform:0.8.6 version
+
+alias terraform='docker run --rm -it -v $HOME/.aws/credentials:/credentials:ro -v $(pwd):/data --workdir=/data hashicorp/terraform:0.8.6'
+
+terraform plan
+terraform apply
+terraform show
+terraform destroy
+
+# inventory
+docker run --rm --workdir=/data -v $(pwd):/data ansible terraform-inventory --inventory
+
+
+ssh-keygen -t rsa -C "aws_instance_rsakey" -P '' -f ssh/aws_instance_rsakey
---
+++ terraform_101.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region     = "eu-west-1"
+  shared_credentials_file = "/credentials"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/ebs-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_key_pair" "aws_instance_rsakey" {
+  key_name = "deployer-key"
+  public_key = "${file("ssh/aws_instance_rsakey.pub")}"
+}
+
+resource "aws_instance" "example" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "m3.medium"
+  key_name = "${aws_key_pair.aws_instance_rsakey.key_name}"
+  count = 3
+
+  tags = {
+    Owner = "ylorenzati"
+  }
+
+  connection {
+    user = "ubuntu"
+    private_key = "${file("ssh/aws_instance_rsakey")}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 1"
+    ]
+  }
+}
