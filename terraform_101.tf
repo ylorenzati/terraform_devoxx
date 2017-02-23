b6cf450925a0aa761e7c281db60bfe9e6a97a896
Yannick Lorenzati <yannick.lorenzat@gmail.com>
2017-02-23 13:47:52 +0100

    add loadbalancing between node

--- terraform_101.tf
+++ terraform_101.tf
@@ -15,20 +15,40 @@ data "aws_ami" "ubuntu" {
 }
 
 
+resource "aws_security_group" "goappsg" {
+  name = "goappsg"
+
+  ingress {
+    from_port = 0
+    to_port = 10000
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  tags = {
+    Owner = "ylorenzati"
+  }
+}
+
 resource "aws_key_pair" "aws_instance_rsakey" {
   key_name = "deployer-key"
   public_key = "${file("ssh/aws_instance_rsakey.pub")}"
 }
 
+
 resource "aws_instance" "example" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "m3.medium"
   key_name = "${aws_key_pair.aws_instance_rsakey.key_name}"
-  count = 1
+  security_groups = [
+    "${aws_security_group.goappsg.name}"]
+  count = 2
+
 
   tags = {
     Owner = "ylorenzati"
-    Name = "yli_terraform"
+    Name = "yli_terraform_instance"
   }
 
   connection {
@@ -40,5 +60,26 @@ resource "aws_instance" "example" {
     inline = [
       "sleep 1"
     ]
+  }
+}
+
+resource "aws_elb" "goapplb" {
+
+  name = "goapplb"
+  availability_zones = [
+    "${element(aws_instance.example.*.availability_zone, count.index)}"]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "tcp"
+    lb_port = 80
+    lb_protocol = "tcp"
+  }
+
+  instances = [
+    "${aws_instance.example.*.id}"]
+
+  tags = {
+    Owner = "ylorenzati"
   }
 }
