e01767841bef0ccd0fcff310a97c5573a6e39d38
Yannick Lorenzati <yannick.lorenzat@gmail.com>
2017-03-31 18:18:17 +0200

    init

--- variables.tf
+++ variables.tf
@@ -1,16 +1,6 @@
 # region: eu-west-2
 # ami:    ami-ed908589
 
-variable "aws_region" {
-  type = "string"
-  default = "eu-west-2"
-}
-
-variable "ami_ubuntu" {
-  type = "string"
-  default = "ami-7effd318"
-}
-
 variable "ami_search_path" {
   type = "string"
   default = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
